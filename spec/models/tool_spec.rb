904070227cc8ae0562c777a65eeca0ef5035ac35
Grant Hall <grantclone@gmail.com>
2023-03-17 19:29:13 +0900

    edited tool tests

---
+++ spec/models/tool_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe Tool, type: :model do
+  let(:tool) {
+    Tool.new(
+      name: "ChatGPT Inspo Machine",
+      note: "this is a machine that gives you AI generated promts for any art piece you want",
+      links: ["https://openai.com/blog/chatgpt"],
+      user: User.create(email: "grant@gmail.com", password: "123123"),
+      internal_categories: "category_placeholder"
+    )
+  }
+
+  it "is valid with all columns filled" do
+    expect(tool.valid?).to eq(true)
+  end
+
+  context 'when missing name' do
+    before do
+      tool.name = ""
+    end
+
+    it "is not valid with a missing name" do
+      expect(tool.valid?).to eq(false)
+    end
+  end
+end
