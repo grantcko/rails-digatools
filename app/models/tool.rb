86e5f7bf4af7bf6a1c736a6c57388b4e0f13f0c9
Grant Hall <grantclone@gmail.com>
2023-05-02 14:18:49 +0900

    made model method private

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -6,6 +6,8 @@ class Tool < ApplicationRecord
   validates :internals, inclusion: { in: ['custom', 'color_picker'], message: "is not a valid item" }, allow_nil: true
   validate :internal_array?
 
+  private
+
   def internal_array?
     unless internals.is_a?(Array)
       errors.add(:my_column, "must be an array")
