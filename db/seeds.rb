5e90cbf2319ba804d23f11cd8f926a1e8952b86b
Grant Hall <grantclone@gmail.com>
2023-05-02 14:45:36 +0900

    wrote very simple seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+user = User.create(email: "grant@gmail.com", password: "123123")
+
+Tool.new(
+  name: "ChatGPT Inspo Machine",
+  note: "this is a machine that gives you AI generated promts for any art piece you want",
+  links: ["https://openai.com/blog/chatgpt"],
+  user: user,
+  internals: ['custom']
+)
