b0a1d62eba5655cd7a598e0b35f2fc67baf2a177
Grant Hall <grantclone@gmail.com>
2023-05-05 12:55:33 +0900

    added tool show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  get '/tools', to: "tools#index", as: :tools
+  # get '/tools', to: "tools#index", as: :tools
+  # get '/tools/:id', to: "tools#"
+  resources :tools, only: %i[index show]
 end
