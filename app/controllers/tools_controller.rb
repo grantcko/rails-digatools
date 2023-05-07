52f79d532ba39ade9dbda2c0e81024941617790c
Grant Hall <grantclone@gmail.com>
2023-05-07 12:15:24 +0900

    add update method for note

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -7,4 +7,31 @@ class ToolsController < ApplicationController
   def show
     @tool = Tool.find(params[:id])
   end
+
+  def create
+    @tool = Tool.new(tool_params)
+
+    if @tool.save
+      redirect_to @tool
+    else
+      status :unprocessable_entity
+      render 'tool'
+    end
+  end
+
+  def update
+    @tool = Tool.find(params[:id])
+    if @tool.update(tool_params)
+      redirect_to @tool
+    else
+      status :unprocessable_entity
+      render 'edit'
+    end
+  end
+
+  private
+
+  def tool_params
+    params.require(:tool).permit(:name, :note, :internals, :links)
+  end
 end
