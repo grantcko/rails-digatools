73a4dbc8926bc77ee5c531c90ede2e4cca4b1a34
Grant Hall <grantclone@gmail.com>
2023-05-14 12:45:06 +0900

    randomize colorpicker on click

--- app/javascript/controllers/picker_controller.js
+++ app/javascript/controllers/picker_controller.js
@@ -1,12 +1,23 @@
 import { Controller } from "@hotwired/stimulus"
+const faker = require('faker');
 
 // Connects to data-controller="picker"
 export default class extends Controller {
   connect() {
+    console.log("working");
     const picker = document.getElementById("picker-box");
-    function logPicker() {
+    function randomizeColor() {
       console.log(picker);
+      const hexCode = document.getElementById("hex-code")
+      if (picker) {
+        let randomColor = faker.internet.color();
+        picker.style.backgroundColor = randomColor;
+        console.log(randomColor);
+        hexCode.innerText = randomColor;
+      } else {
+        console.log("The element with the ID 'picker-box' does not exist.");
+      }
     }
-    picker.addEventListener("click", logPicker);
+    picker.addEventListener("click", randomizeColor);
   }
 }
