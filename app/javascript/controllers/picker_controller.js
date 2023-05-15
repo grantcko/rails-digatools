ca2484f32c3d5da80a8262043f49d7bedc5dfb11
Grant Hall <grantclone@gmail.com>
2023-05-15 14:04:55 +0900

    added click to copy

--- app/javascript/controllers/picker_controller.js
+++ app/javascript/controllers/picker_controller.js
@@ -4,21 +4,36 @@ const faker = require('faker');
 // Connects to data-controller="picker"
 export default class extends Controller {
   connect() {
-    console.log("working");
     const picker = document.getElementById("picker-box");
+    const hexCopyBar = document.querySelector(".hex-copy-bar");
+    const hexCode = document.getElementById("hex-code");
+    const copyNotifications = document.querySelectorAll(".copy-notification");
+
     function randomizeColor() {
-      console.log(picker);
-      const hexCode = document.getElementById("hex-code")
       if (picker) {
         let randomColor = faker.internet.color();
         picker.style.backgroundColor = randomColor;
-        console.log(randomColor);
         hexCode.innerText = randomColor;
       } else {
         console.log("The element with the ID 'picker-box' does not exist.");
       }
     }
 
+    function copyText() {
+      navigator.clipboard.writeText(hexCode.innerText);
+      copyNotifications.forEach(copyNotification => {
+        copyNotification.innerText = "copied!";
+      });
+
+      setTimeout(() => {
+        copyNotifications.forEach(copyNotification => {
+          console.log("timeout completed");
+          copyNotification.innerText = "click to copy"
+        });
+      }, "2000");
+    }
+
     picker.addEventListener("click", randomizeColor);
+    hexCopyBar.addEventListener("click", copyText);
   }
 }
