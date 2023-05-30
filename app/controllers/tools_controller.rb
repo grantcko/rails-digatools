dac315f9df4e8d6b47753204271585c389cc15c7
Grant Hall <grantclone@gmail.com>
2023-05-30 18:18:49 +0900

    working eq

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -13,7 +13,7 @@ class ToolsController < ApplicationController
     @select_eq_direction = params[:direction]
     @select_audio_input = params[:file]
     if !@select_eq_direction.nil? || !@select_audio_input.nil?
-      equalize_audio(@select_audio_input, @select_eq_direction.to_sym)
+      @eq_method_test = equalize_audio(@select_audio_input, @select_eq_direction.to_sym)
     end
   end
 
@@ -40,30 +40,39 @@ class ToolsController < ApplicationController
 
   # Get audio file and apply eq changes based on direction
   def equalize_audio(audio_input, direction)
-    # define file name and extension then append "_output" to the file
-    file_name = File.basename(audio_input)
-    file_ext = file_name.match(/\.(?<extension>\w+)$/)[:extension]
-    output_file_name = "#{file_name}_output.#{file_ext}"
+    # define file name as the path of the input unless input is a string
+    if audio_input.is_a?(String)
+      file_name = audio_input
+    else
+      file_name = audio_input.path
+    end
+
+    # define basename and file extension
+    file_base_name = File.basename(audio_input)
+    file_ext = file_base_name.match(/\.(?<extension>\w+)$/)[:extension]
+
+    # append "_output" to the file
+    output_file_name = "#{file_base_name}_output.#{file_ext}"
 
     # return error if invalid direction or audio_input
     raise ArgumentError.new("Invalid audio_input") unless valid_audio_input?(file_ext)
     raise ArgumentError.new("Invalid direction") unless valid_direction?(direction)
-
     # apply eq changes based on direction
     case direction
     when EQ_DIRECTIONS[0] # highpass
       # apply HIGHPASS changes with ffmpeg cli (both channels, at 0Hz, 3000Hz wide, -96dB )
-      system("ffmpeg -i #{audio_input} -af 'anequalizer=c0 f=0 w=3000 g=-96 t=2|c1 f=0 w=3000 g=-96 t=2' storage/equalized_audio/#{output_file_name}")
+      system("ffmpeg -i #{file_name} -af 'anequalizer=c0 f=0 w=3000 g=-96 t=2|c1 f=0 w=3000 g=-96 t=2' storage/equalized_audio/#{output_file_name}")
     when EQ_DIRECTIONS[1] # radio
       # apply RADIO changes with ffmpeg cli (both channels, at Hz, Hz wide, -dB )
-      system("ffmpeg -i #{audio_input} -af 'anequalizer=c0 f=0 w=2500 g=-96 t=2|c1 f=0 w=2500 g=-96 t=2|c0 f=15000 w=16000 g=-96 t=2|c1 f=15000 w=16000 g=-96 t=2' storage/equalized_audio/#{output_file_name}")
+      system("ffmpeg -i #{file_name} -af 'anequalizer=c0 f=0 w=2500 g=-96 t=2|c1 f=0 w=2500 g=-96 t=2|c0 f=15000 w=16000 g=-96 t=2|c1 f=15000 w=16000 g=-96 t=2' storage/equalized_audio/#{output_file_name}")
     when EQ_DIRECTIONS[2] # lowpass
       # apply lowpass changes
-      system("ffmpeg -i #{audio_input} -af 'anequalizer=c0 f=15000 w=16000 g=-96 t=2|c1 f=15000 w=16000 g=-96 t=2' storage/equalized_audio/#{output_file_name}")
+      system("ffmpeg -i #{file_name} -af 'anequalizer=c0 f=15000 w=16000 g=-96 t=2|c1 f=15000 w=16000 g=-96 t=2' storage/equalized_audio/#{output_file_name}")
     when EQ_DIRECTIONS[3] # vocal
       # apply vocal changes
-      system("ffmpeg -i #{audio_input} -af 'anequalizer=c0 f=3000 w=2000 g=10 t=2|c1 f=3000 w=2000 g=10 t=2' storage/equalized_audio/#{output_file_name}")
+      system("ffmpeg -i #{file_name} -af 'anequalizer=c0 f=3000 w=2000 g=10 t=2|c1 f=3000 w=2000 g=10 t=2' storage/equalized_audio/#{output_file_name}")
     end
+    return "storage/equalized_audio/#{output_file_name}"
   end
 
   private
