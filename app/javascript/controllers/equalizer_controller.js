6df2f87355e3b72ae74efaff59a3007211b74a66
Grant Hall <grantclone@gmail.com>
2023-06-23 18:38:44 +0900

    gif load on click

--- app/javascript/controllers/equalizer_controller.js
+++ app/javascript/controllers/equalizer_controller.js
@@ -5,17 +5,21 @@ export default class extends Controller {
   connect() {
     const container = document.getElementById('equalizer-container');
     const downloadLink = document.createElement('a');
+
     // select auto equalizer form
     const eqForm = document.getElementById("equalize-form");
+    // const eqFormSubmit = document.getElementById("eq-submit");
+    const loader = document.getElementById("loader");
 
     // run fuction when user submits the form
-    eqForm.addEventListener("submit", submit,);
+    eqForm.addEventListener("submit", submit);
 
     function submit(event) {
+      // add loader
       // send form data to server for eq processing + add link to the output file
       console.log("submitted");
+      loader.style.display = 'block'
       event.preventDefault();
-
       const formData = new FormData(event.target);
       fetch("/equalize", {method: "POST", body: formData})
       .then(function(response) {
@@ -24,15 +28,16 @@ export default class extends Controller {
         return response.json();
       })
       .then(function(data) {
-        console.log(`this is the output file name: ${data.output}`);
-        downloadLink.textContent = `⬇️ ${data.output} ⬇️`;
-        const url = new URL(window.location.href);
-        const tool_id = String(url.pathname).match(/\/tools\/(\d+)/)[1]
-
-        downloadLink.href = `/download/?file=${data.output}&id=${tool_id}`;
-        console.log(`this is the download link: ${downloadLink}`);
-        container.insertBefore(downloadLink, eqForm)
-      });
+        // remove loader
+          loader.style.display = 'none'
+          console.log(`this is the output file name: ${data.output}`);
+          downloadLink.textContent = `⬇️ ${data.output} ⬇️`;
+          const url = new URL(window.location.href);
+          const tool_id = String(url.pathname).match(/\/tools\/(\d+)/)[1]
+          downloadLink.href = `/download/?file=${data.output}&id=${tool_id}`;
+          console.log(`this is the download link: ${downloadLink}`);
+          container.insertBefore(downloadLink, eqForm)
+        });
     };
   }
 }
