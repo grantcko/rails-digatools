1e512aad7a8f4341bd244265f3b2bfba710caec3
Grant Hall <grantclone@gmail.com>
2023-06-29 19:17:41 +0900

    added demo page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get '/tools/demos', to: "tools#demo_index", as: :demos
+  get '/tools/demos/:internal', to: "tools#demo_show", as: :demo
   resources :tools, only: %i[index show create new update destroy]
   post '/equalize', to: "tools#equalize_audio", as: :equalize
   get '/download', to: "tools#download", as: :download
