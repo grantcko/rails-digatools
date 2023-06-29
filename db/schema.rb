9b60adc615792055e7a1db7f53222766e679b1cb
Grant Hall <grantclone@gmail.com>
2023-06-29 19:18:17 +0900

    added column to tools

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_20_041852) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_29_080026) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_20_041852) do
     t.datetime "updated_at", null: false
     t.string "internals", default: [], array: true
     t.string "links", default: [], array: true
+    t.integer "column"
     t.index ["user_id"], name: "index_tools_on_user_id"
   end
 
